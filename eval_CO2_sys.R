0031092e39d28f9ee085c5d5b5c9746625e4be9b
Martin <martwine@gmail.com>
2012-11-15 13:53:50 +0000

    CO2 system

---
+++ eval_CO2_sys.R
@@ -0,0 +1,10 @@
+# evaluate CO2 system parameters given TA and DIC, salinity and temperature
+
+# this turned out to be easy thanks to the seacarb package! More might end up in here later
+library("seacarb")
+
+eval_CO2_sys<-function(TA,DIC,S,T){
+	return carb(flag=15, var1=0.002, var2=0.002, S=35, T=25, P=0, Pt=0, Sit=0, k1k2="x", kf="x", ks="d", pHscale="T", b="l10")
+}
+
+
