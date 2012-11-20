15130464b64d47cf0d83e8ce74c777badf5d29e2
Martin Johnson <martwine@gmail.com>
2012-11-20 23:43:36 +0000

    separated model run from config and defined default config

--- core_model.R
+++ core_model.R
@@ -5,78 +5,48 @@ library("seacarb")
 source("K_calcs_Johnson_OS.R")
 
 
-
-
-###############################################################################
-###### physical set-up ########################################################
-
-#surface mixed layer depth
-sMLD<-20 #m
-
-#water column depth
-column_depth<-100 #m
-
 #calc bottom mixed layer depth
-bMLD<-column_depth-sMLD
+BMLD<-COLUMN_DEPTH-SMLD
 
 ################################################################################################
 ###############    Boundary conditions  ########################################################
 
 
-#temperature is important for respiration rates and CO2 system parameters, also air-sea flux
+#Temperature cycle calculated from max and min values (1st March min, 1st September max)
 
-#min_surface_temp<-4
-#max_surface_temp<-16
+surface_temp_cycle<-MIN_SURFACE_TEMP+(0.5*(MAX_SURFACE_TEMP-MIN_SURFACE_TEMP))*(1+sin((seq(from=-90,to=270,length.out=365)*pi)/180))
 
-min_surface_temp<-10
-max_surface_temp<-10
+surface_temp<-c(surface_temp_cycle[(365-OFFSET):365],surface_temp_cycle[1:(365-OFFSET-1)])
 
 
-
-#initially let's just have a sine wave temp
-
-surface_temp<-min_surface_temp+(0.5*(max_surface_temp-min_surface_temp))*(1+sin((seq(from=-90,to=270,length.out=365)*pi)/180))
-
-
-
-#nitrate concentrations drive the model initally. Spring bloom is a simple linear decrease from winter nitrate to zero, where start date and duration are defined
-
-winter_nitrate<-12 #uM
-bloom_start_day<-88 #jday
-bloom_duration<-15 #days
-
-#summer length defines when the BML and SML are mixed into a single box again
-summer_length<-150 #days
 
 #define run_length as from 1st Jan 1 year, to immediately before spring bloom the next
-run_length=365+bloom_start_day
+run_length=365+BLOOM_START_DAY
 
 
 #calc jday of mixing event
-mix_day<-bloom_start_day+bloom_duration+summer_length
+mix_day<-BLOOM_START_DAY+BLOOM_DURATION+SUMMER_LENGTH
 
 #define surface nitrate 'envelope'
-nday<-c(1,bloom_start_day,bloom_start_day+bloom_duration,run_length)
-nitrate<-c(winter_nitrate,winter_nitrate,0,0)
+nday<-c(1,BLOOM_START_DAY,BLOOM_START_DAY+BLOOM_DURATION,run_length)
+nitrate<-c(WINTER_NITRATE,WINTER_NITRATE,0,0)
 nitrate<-approx(nday,nitrate,n=run_length)
 
 
-#winter atmospheric pCO2 North Sea (est from Weybourne (Phil Wilson))
-
-winter_pCO2_atmos<-397
-
 #get a simple seasonal cycle of atmospheric CO2
-average_pCO2_atmos<-390
-min_pCO2<-average_pCO2_atmos-(winter_pCO2_atmos-average_pCO2_atmos)
-#pCO2_atmos<-(min_pCO2+7*(1+cos((seq(from=0,to=360,length.out=365)*pi)/180)))
-pCO2_atmos<-seq(from=average_pCO2_atmos, to=average_pCO2_atmos,length.out=365) #constant pCO2 for reference run
+min_pCO2<-AVERAGE_pCO2-(AMPLITUDE/2)
+pCO2_atmos<-(min_pCO2+(AMPLITUDE/2)*(1+cos((seq(from=0,to=360,length.out=365)*pi)/180)))
+
+init_pCO2<-min_pCO2+AMPLITUDE
 
 
-#estimate of TA from artioli et al 2012
-init_TA<-2300 #uM
+#initalise DIC from pCO2 and estimated TA 
+init_DIC<-1e6*carb(flag=24,init_pCO2-delta_pCO2,init_TA*1e-6)$DIC[1]
 
-#initalise DIC from pCO2 and estimated TA assuming equilibration
-init_DIC<-1e6*carb(flag=24,winter_pCO2_atmos,init_TA*1e-6)$DIC[1]
+
+#wind speed cycle
+wind_speed<-MIN_WIND+(0.5*(MAX_WIND-MIN_WIND))*(1+sin((seq(from=90,to=450,length.out=365)*pi)/180))
+
 
 
 #initalise_data
@@ -84,45 +54,22 @@ box<-as.data.frame(nitrate)
 colnames(box)<-c("day","nitrate")
 box$pCO2_atmos<-c(pCO2_atmos,pCO2_atmos)[1:run_length]
 box$temp<-c(surface_temp,surface_temp)[1:run_length]
-#delta nitrate column
+box$wind<-c(wind_speed,wind_speed)[1:run_length]
+
+#delta nitrate column - gives change between each timestep and previous
 box$dNO3<-c(0,diff(box$nitrate))
 
-#make a second box (BML) which simply respires all the POM at redfield and mixes back in 
-
-##########################################################################################
-################  Parameter values  ######################################################
-##########################################################################################
-
-redfield<-6.6 #redfieldian C:N
-
-# ammonium turnover rate
-#rho_NH4<-0.25 #uM day-1
-
-# C:N ratio of semi-labile DOM produced during bloom
-sl_DOM_CtoN<-redfield
-
-# Proportion of spring bloom N turned into semi-labile DON
-nitrate_slDOM_conv<-0
-
-# degradation rate of sl_DON
-sl_DON_deg<-0.02 #day-1
-
-#degradation rate of sl_DOC
-sl_DOC_deg<-0.01 #day-1
-
-#air-sea CO2 flux params
-k_w<-2.9 #m day-1
 
 
 #########################################################################################
 ############## process and state variable evaluation functions ##########################
 #########################################################################################
 
-calc_as_flux<-function(pCO2,pCO2_atmos,temp){
-	delta_pCO2<-KH_Molar_per_atmosphere("CO2",temp,S=35)*(pCO2_atmos-pCO2)*1e-6 #mol/l
-	flux<-1e6*delta_pCO2*1000*k_w #umol/m2/day
-
-	
+calc_as_flux<-function(pCO2,pCO2_atmos,temp,wind){
+	deltapCO2<-KH_Molar_per_atmosphere("CO2",temp,S=35)*(pCO2_atmos-pCO2)*1e-6 #mol/l
+	k_w<-Nightingkw("CO2",temp,wind,35)*3600*24
+	flux<-1e6*deltapCO2*1000*k_w #umol/m2/day
+	flux
 }
 
 calc_DIC_uptake_from_NO3<-function(dNO3){
@@ -130,30 +77,30 @@ calc_DIC_uptake_from_NO3<-function(dNO3){
 }
 
 calc_slDON_deg<-function(slDON){
-	slDON*sl_DON_deg
+	slDON*slDON_deg
 }
 
 
 calc_slDOC_deg<-function(slDOC){
-	slDOC*sl_DOC_deg
+	slDOC*slDOC_deg
 }
 
 calc_prod_slDON<-function(dNO3){
-	-dNO3*nitrate_slDOM_conv
+	-dNO3*nitrate_to_slDON_conv
 }
 
 calc_prod_slDOC<-function(dNO3){
-	calc_prod_slDON(dNO3)*sl_DOM_CtoN
+	calc_prod_slDON(dNO3)*slDOM_C_TO_N
 }
 
 
 #instantly remineralise POM in the bottom layer
 calc_NO3_BML<-function(dNO3){
-	-dNO3*(1-nitrate_slDOM_conv)*sMLD/bMLD
+	-dNO3*(1-nitrate_to_slDON_conv)*SMLD/BMLD
 }
 
 calc_DIC_BML<-function(dNO3){
-	-dNO3*(1-nitrate_slDOM_conv)*redfield*sMLD/bMLD
+	-dNO3*(1-nitrate_to_slDON_conv)*redfield*SMLD/BMLD
 }
 
 
@@ -165,8 +112,8 @@ eval_slDOC<-function(dNO3, slDOC){
 	slDOC+calc_prod_slDOC(dNO3)-calc_slDOC_deg(slDOC)
 }
 
-eval_DIC<-function(DIC,dNO3,pCO2,pCO2_atmos,temp,slDOC,depth){
-	DIC-calc_DIC_uptake_from_NO3(dNO3)+((calc_as_flux(pCO2,pCO2_atmos,temp)/depth)/1000)+calc_slDOC_deg(slDOC)
+eval_DIC<-function(DIC,dNO3,pCO2,pCO2_atmos,temp,slDOC,depth,wind){
+	DIC-calc_DIC_uptake_from_NO3(dNO3)+((calc_as_flux(pCO2,pCO2_atmos,temp,wind)/depth)/1000)+calc_slDOC_deg(slDOC)
 }
 
 eval_BML_DIC<-function(dNO3, BML_DIC){
@@ -178,7 +125,7 @@ eval_BML_NO3<-function(dNO3, BML_NO3){
 }
 
 calc_mix<-function(sml_conc,bml_conc){
-	(sml_conc*sMLD + bml_conc*bMLD)/column_depth
+	(sml_conc*SMLD + bml_conc*BMLD)/COLUMN_DEPTH
 }
 
 
@@ -191,6 +138,7 @@ eval_timestep<-function(timestep,current_state){
 	pCO2_atmos<-timestep_row$pCO2_atmos
 	dNO3<-timestep_row$dNO3
 	temp<-timestep_row$temp
+	wind<-timestep_row$wind
 
 	DIC<-current_state$DIC
 	slDOC<-current_state$slDOC
@@ -203,11 +151,11 @@ eval_timestep<-function(timestep,current_state){
 	if(timestep<mix_day){
 		stepdata$slDOC<-eval_slDOC(dNO3, slDOC)
 		stepdata$slDON<-eval_slDON(dNO3, slDON)
-		stepdata$airseaFlux<-calc_as_flux(pCO2,pCO2_atmos,temp)
-		if(timestep<bloom_start_day){
-			stepdata$DIC<-eval_DIC(DIC,dNO3,pCO2,pCO2_atmos,temp,slDOC,depth=column_depth)
+		stepdata$airseaFlux<-calc_as_flux(pCO2,pCO2_atmos,temp,wind)
+		if(timestep<BLOOM_START_DAY){
+			stepdata$DIC<-eval_DIC(DIC,dNO3,pCO2,pCO2_atmos,temp,slDOC,depth=COLUMN_DEPTH,wind)
 		} else {
-			stepdata$DIC<-eval_DIC(DIC,dNO3,pCO2,pCO2_atmos,temp,slDOC,depth=sMLD)
+			stepdata$DIC<-eval_DIC(DIC,dNO3,pCO2,pCO2_atmos,temp,slDOC,depth=SMLD,wind)
 		}		
 		stepdata$pCO2<-carb(flag=15,init_TA*1e-6,stepdata$DIC*1e-6)$pCO2[1]
 		stepdata$BML_DIC<-eval_BML_DIC(dNO3, BML_DIC)
@@ -223,8 +171,8 @@ eval_timestep<-function(timestep,current_state){
 		#then run the timestep
 		stepdata$slDOC<-eval_slDOC(dNO3, mixed_slDOC)
 		stepdata$slDON<-eval_slDON(dNO3, mixed_slDON)
-		stepdata$airseaFlux<-calc_as_flux(mixed_pCO2,pCO2_atmos,temp)
-		stepdata$DIC<-eval_DIC(mixed_DIC,dNO3,mixed_pCO2,pCO2_atmos,temp,slDOC,depth=column_depth)
+		stepdata$airseaFlux<-calc_as_flux(mixed_pCO2,pCO2_atmos,temp,wind)
+		stepdata$DIC<-eval_DIC(mixed_DIC,dNO3,mixed_pCO2,pCO2_atmos,temp,slDOC,depth=COLUMN_DEPTH,wind)
 		stepdata$pCO2<-carb(flag=15,init_TA*1e-6,stepdata$DIC*1e-6)$pCO2[1]
 		stepdata$BML_DIC<-stepdata$DIC
 		stepdata$BML_NO3<-mixed_NO3			
@@ -234,8 +182,8 @@ eval_timestep<-function(timestep,current_state){
 	} else {
 		stepdata$slDOC<-eval_slDOC(dNO3, slDOC)
 		stepdata$slDON<-eval_slDON(dNO3, slDON)
-		stepdata$airseaFlux<-calc_as_flux(pCO2,pCO2_atmos,temp)
-		stepdata$DIC<-eval_DIC(DIC,dNO3,pCO2,pCO2_atmos,temp,slDOC,depth=column_depth)
+		stepdata$airseaFlux<-calc_as_flux(pCO2,pCO2_atmos,temp,wind)
+		stepdata$DIC<-eval_DIC(DIC,dNO3,pCO2,pCO2_atmos,temp,slDOC,depth=COLUMN_DEPTH,wind)
 		stepdata$pCO2<-carb(flag=15,init_TA*1e-6,stepdata$DIC*1e-6)$pCO2[1]
 		stepdata$BML_DIC<-stepdata$DIC
 		stepdata$BML_NO3<-BML_NO3
@@ -247,13 +195,13 @@ eval_timestep<-function(timestep,current_state){
 
 
 ################################################################################
-#     model control                                                            #
+#     model control                                              #
 ################################################################################
 
 model_run<-function(run_name){
 
 	timestep = 1
-	model_output<-data.frame(pCO2=winter_pCO2_atmos,DIC=init_DIC,slDON=0,slDOC=0,airseaFlux=0,BML_DIC=init_DIC, BML_NO3=winter_nitrate)
+	model_output<-data.frame(pCO2=init_pCO2,DIC=init_DIC,slDON=0,slDOC=0,airseaFlux=0,BML_DIC=init_DIC, BML_NO3=WINTER_NITRATE)
 
 	
 	while(timestep<run_length){
@@ -278,6 +226,6 @@ model_run<-function(run_name){
 	model	
 }
 
-model_run("two-box ref run 1")
+model_run(RUN_NAME)
 
 
---
+++ default_config.R
@@ -0,0 +1,83 @@
+#################################################################
+### DEFAULT CONFIG FILE FOR Shelf Sea Biogeochemistry  model ####
+#################################################################
+
+
+RUN_NAME<-"DEFAULT"
+
+## mode can be 1-box (surface) or 2-vertical boxes (surface and 
+## depth), which are mixed for the winter
+
+#mode=1
+MODE=2
+
+
+#################################
+##### WATER COLUMN DEPTHS   #####
+#################################
+
+## surface mixed layer depth
+SMLD<-20
+
+## water column depth
+COLUMN_DEPTH<-100
+
+##################################
+###### BOUNDARY CONDITIONS  ######
+##################################
+
+## sine wave temperature variation defined from these values
+
+MIN_SURFACE_TEMP<-4
+MAX_SURFACE_TEMP<-16
+
+OFFSET<-59 # days to 1st March (temperature min date)
+
+## sine wave pCO2 based based on Weybourne data (from Phil Wilson)
+## pCO2 max in Jan, min in July
+AVERAGE_pCO2<-390 #uatm
+AMPLITUDE<-14 #uatm
+
+## sine wave wind speed seems to reasonably represent North Sea winds e.g. http://www.windfinder.com/windstats/windstatistic_humber_1_northsea.htm (1 knot = 0.5144 m/s) max Jan, min July
+MIN_WIND<-7.2 #m/s
+MAX_WIND<-11.3 #m/s
+
+## model is initialised with winter nitrate and the bloom period is described by start day and length to start the model off
+
+WINTER_NITRATE<-12 #uM
+BLOOM_START_DAY<-88 #jday
+BLOOM_DURATION<-15 #days
+
+## summer length defines when the BML and SML are mixed into a single box again (they separate at the onset of the spring bloom)
+SUMMER_LENGTH<-150 #days
+
+## estimate of total alkalinity from artioli et al 2012
+init_TA<-2300 #uM
+
+## delta pCO2 defines starting disequilibium between atmos and surface ocean (+ve indicates pCO2 atmos > pCO2 ocean)
+delta_pCO2<-0
+
+
+##################################
+######## PARAMETER VALUES ########
+##################################
+
+# ammonium turnover rate
+#rho_NH4<-0.25 #uM day-1
+
+redfield<-6.6 #C:N for 'Redfieldia`' processes
+
+## C:N ratio of semi-labile DOM produced during bloom
+slDOM_C_TO_N<-redfield
+
+## Proportion of spring bloom N turned into semi-labile DON
+nitrate_to_slDON_conv<-0
+
+# degradation rate of sl_DON
+slDON_deg<-0.02 #day-1
+
+#degradation rate of sl_DOC
+slDOC_deg<-0.01 #day-1
+
+
+
