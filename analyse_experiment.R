b0e8a742737dd700aab5fb45a575dce8f551fa00
Martin <martwine@gmail.com>
2012-12-03 16:58:04 +0000

    er

--- analyse_experiment.R
+++ analyse_experiment.R
@@ -2,8 +2,8 @@
 ####### analyse data from lhc experiment ####################
 #############################################################
 
-experiment_name="physical_pump_lhc"
-experiment_nruns=10000
+experiment_name="test_experiment"
+experiment_nruns=10
 experiment_path=paste("experiments/",experiment_name,"/",sep="")
 
 
@@ -20,16 +20,19 @@ trim_param<-function(param){
 get_params<-function(run_config_file){
 	params<-readLines(run_config_file)
 	d<-as.list(unlist(lapply(params,trim_param)))
+	print(d$run_number)
 	d$run_number<-NULL
-	d	
+	as.data.frame(t(unlist(d)))	
 }
 
 parse_data<-function(run_data_file){
 	run_data<-read.csv(run_data_file)
-	list(
+	print(run_data_file)
+	x<-list(
 		asflux=tail(cumsum(run_data$airseaFlux),n=1),
 		delta_total_carbon=tail(run_data$total_C,n=1)-run_data$total_C[2]
 	)
+	as.data.frame(t(unlist(x)))
 	
 }
 
@@ -41,9 +44,7 @@ experiment_data$run_config_file<-paste(experiment_path,experiment_name,"_",exper
 experiment_data$run_data_file<-paste(experiment_path,experiment_name,"_",experiment_data$run_number,".csv",sep="")
 experiment_data<-cbind(experiment_data, t(sapply(experiment_data$run_config_file,get_params)),t(sapply(experiment_data$run_data_file,parse_data)))
 
-cbind(experiment_data,get_params(experiment_data$run_config_file),parse_data(experiment_data$run_data_file))
-
-
+save(experiment_data,file=paste("analysis",experiment_name,"_summary.Rdata",sep=""))
 
 
 
